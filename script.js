a01f4d938b41d72d8e913809dfeb66be995cda31
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2021-12-05 21:39:03 -0600

    final changes

--- script.js
+++ script.js
@@ -1,46 +1,22 @@
 function initPage() {
-    // Get the current hour of the day
     const currentHour = parseInt(moment().format("kk"));
-    // Update the page with the current date
     const currentDayEl = document.getElementById("currentDay");
     currentDayEl.innerHTML = moment().format("dddd, MMMM Do YYYY");
-
-    // Initialize a new array of note objects if none exists in local storage
     const emptyPlan = [
-        {
-            "time": 9, "note": ""
-        },
-        {
-            "time": 10, "note": ""
-        },
-        {
-            "time": 11, "note": ""
-        },
-        {
-            "time": 12, "note": ""
-        },
-        {
-            "time": 13, "note": ""
-        },
-        {
-            "time": 14, "note": ""
-        },
-        {
-            "time": 15, "note": ""
-        },
-        {
-            "time": 16, "note": ""
-        },
-        {
-            "time": 17, "note": ""
-        }
+        {"time": 9, "note": ""},
+        {"time": 10, "note": ""},
+        {"time": 11, "note": ""},
+        {"time": 12, "note": ""},
+        {"time": 13, "note": ""},
+        {"time": 14, "note": ""},
+        {"time": 15, "note": ""},
+        {"time": 16, "note": ""},
+        {"time": 17, "note": ""}
     ];
-
     const dayPlan = JSON.parse(localStorage.getItem("plan")) || emptyPlan;
     const timeblockContainer = document.getElementById("timeblock-container");
     timeblockContainer.innerHTML = "";
 
-    // Dynamically generate the HTML for all time blocks
     for (let i=0; i<dayPlan.length; i++) {
         let hourString;
         const hour = 9 + i;
@@ -54,19 +30,16 @@ function initPage() {
         const timeBlock = document.createElement("div");
         timeBlock.setAttribute("class", "row input-group time-block");
         timeblockContainer.append(timeBlock);
-
         const timeContainer = document.createElement("div");
         timeContainer.setAttribute("class", "input-group-prepend");
         timeBlock.append(timeContainer);
-
         const timeSpan = document.createElement("span");
         timeSpan.setAttribute("class", "input-group-text hour");
         timeSpan.innerHTML = hourString;
         timeContainer.append(timeSpan);
-
         const noteEl = document.createElement("input");
         noteEl.setAttribute("type", "text");
-        // Compare the time block with the current time to decide what style it should have
+
         if (hour < currentHour) {
             noteEl.setAttribute("class", "form-control description past");
         } else if (hour === currentHour) {
@@ -76,18 +49,14 @@ function initPage() {
         }
         noteEl.setAttribute("placeholder", dayPlan[i].note);
         timeBlock.append(noteEl);
-
         const saveContainer = document.createElement("div");
         saveContainer.setAttribute("class", "input-group-append");
         timeBlock.append(saveContainer);
-
         const saveSpan = document.createElement("span");
         saveSpan.setAttribute("class", "saveBtn");
         saveContainer.append(saveSpan);
-
         const saveIcon = document.createElement("i");
         saveIcon.setAttribute("class", "fas fa-save");
-        // Save button saves note entered by user to local storage
         saveIcon.addEventListener("click", () => {
             dayPlan[i].note = noteEl.value;
             localStorage.setItem("plan", JSON.stringify(dayPlan));
